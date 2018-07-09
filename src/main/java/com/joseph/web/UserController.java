94679fae72ae40638a581b67c33c00b3c614d1f8
josephkwon <devel.kwon@gmail.com>
2018-07-09 18:00:04 +0900

    로그인 기능 구현 완료

--- src/main/java/com/joseph/web/UserController.java
+++ src/main/java/com/joseph/web/UserController.java
@@ -3,6 +3,8 @@ package com.joseph.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,6 +24,29 @@ public class UserController {
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		return "/user/login";
+	}
+	
+	@PostMapping("login")
+	public String login(String userId, String password, HttpSession session) {
+		User user = userRepository.findByUserId(userId);
+		if (user == null) {
+			System.out.println("Login Failure!");
+			return "redirect:/users/loginForm";
+		}
+		if (!password.equals(user.getPassword())) {
+			System.out.println("Login Failure!");
+			return "redirect:/users/loginForm";
+		}
+		
+		System.out.println("Login Success!");
+		session.setAttribute("user", user);
+		
+		return "redirect:/";
+	}
 	
 	@GetMapping("/form")
 	public String form() {
@@ -55,4 +80,6 @@ public class UserController {
 		userRepository.save(user);
 		return "redirect:/users";
 	}
+	
+	
 }

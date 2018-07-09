c1c330aa812fb9d5caf8dd10c90ada6dfbc9a886
josephkwon <devel.kwon@gmail.com>
2018-07-09 09:48:55 +0900

    개인정보 수정 기능 추가

---
+++ src/main/java/com/joseph/web/UserController.java
@@ -0,0 +1,58 @@
+package com.joseph.web;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.joseph.domain.User;
+import com.joseph.domain.UserRepository;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+	private List<User> users = new ArrayList<User>();
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@GetMapping("/form")
+	public String form() {
+		return "/user/form"; 
+	}	
+	
+	@PostMapping("")
+	public String create(User user) {
+		System.out.println("user : " + user);
+		userRepository.save(user);
+		return "redirect:/users";
+	}
+
+	@GetMapping("")
+	public String list(Model model) {
+		model.addAttribute("user", userRepository.findAll());
+		return "/user/list"; 
+	}
+
+	@GetMapping("{id}/form")
+	public String updateForm(@PathVariable Long id, Model model) {
+		User user = userRepository.findOne(id);
+		model.addAttribute("user", user);
+		return "/user/updateForm"; 
+	}		
+	
+	@PutMapping("/{id}")
+	public String update(@PathVariable Long id, User newUser) {
+		User user = userRepository.findOne(id);
+		user.update(newUser);
+		userRepository.save(user);
+		return "redirect:/users";
+	}
+}
---
+++ src/main/java/com/joseph/web/WelcomeController.java
@@ -0,0 +1,23 @@
+package com.joseph.web;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+	@GetMapping("/helloworld")
+	public String welcome(Model model) {
+		//System.out.println("name : " + name + " age : " + age);
+		//model.addAttribute("name", name );
+		//model.addAttribute("age", age );
+		List<MyModel> repo = Arrays.asList(new MyModel("javajigi"), new MyModel("sanjigi"), new MyModel("joseph"));
+		model.addAttribute("repo", repo);		
+		
+
+		return "welcome";
+	}
+}

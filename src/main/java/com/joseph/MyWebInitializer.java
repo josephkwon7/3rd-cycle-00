91a31aba0e35dac592ddf6415c8e357f7a6f4138
josephkwon <devel.kwon@gmail.com>
2018-07-10 17:33:05 +0900

    외부 tomcat에 war 형식으로 배포하도록 추가함

---
+++ src/main/java/com/joseph/MyWebInitializer.java
@@ -0,0 +1,12 @@
+package com.joseph;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class MyWebInitializer extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MyApplication.class);
+	}
+
+}

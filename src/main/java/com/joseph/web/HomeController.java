0a7b44818998fb1988889eb8c64c44b0476e8e2d
josephkwon <devel.kwon@gmail.com>
2018-07-10 12:20:55 +0900

    질문 후 리스트 보기 기능 구현 완료

--- src/main/java/com/joseph/web/HomeController.java
+++ src/main/java/com/joseph/web/HomeController.java
@@ -1,12 +1,20 @@
 package com.joseph.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.joseph.domain.QuestionRepository;
 
 @Controller
 public class HomeController {
+	@Autowired
+	private QuestionRepository questionRepository;
+	
 	@GetMapping("")
-	public String home() {
+	public String home(Model model) {
+		model.addAttribute("questions", questionRepository.findAll());
 		return "index";
 	}
 }
